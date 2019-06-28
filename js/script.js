06d98ab0dab8c308744f338ba1684ea7a53936ca
mattoyinbo <mattoyinbo@gmail,com>
2019-06-27 20:08:08 -0400

    code cleanup

--- js/script.js
+++ js/script.js
@@ -1,22 +1,6 @@
 (()=>{ 
+
 //Array of colors
-
-
-//Function to genrate random Hexcolor 
-function gethexValue(){
-
-//Array of posible Hex values
-    let hexVal = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-    let hexColor = [];
-    for(i=0;i<6;i++){
-        let tempVal =  hexVal[Math.floor(Math.random() * hexVal.length)];
-        hexColor[i] = tempVal;
-    }
-    let hexFinal = hexColor.join('');
-    hexFinal = '#' + hexFinal;
-    return hexFinal;
-     
-}
 // const colors = ['#444444','#555555','#777777','#FF5E5B','#D8D8D8','#BA2C73'];
 
 
@@ -24,9 +8,7 @@ function gethexValue(){
 const btn = document.getElementById('btn');  
 const hexValue = document.getElementById('hex');
 
-
 // let backHexColor = gethexValue();
-
 //add event listener of type click
 btn.addEventListener('click',()=>{
 
@@ -46,13 +28,35 @@ btn.addEventListener('click',()=>{
     // hexFinal = '#' + hexFinal;
 
     //function call to generate Hexcolor
+
     let hexFinal2 = gethexValue()
     document.body.style.backgroundColor = hexFinal2;
     hexValue.textContent = `Hexcolor: ${hexFinal2}`;
     hexValue.style.backgroundColor = gethexValue();
     console.log(hexFinal2);
-    
+       
 })
+
+//Function to genrate random Hexcolor 
+function gethexValue(){
+
+    //Array of posible Hex values
+        let hexVal = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+        let hexColor = [];
+        for(i=0;i<6;i++){
+            let tempVal =  hexVal[Math.floor(Math.random() * hexVal.length)];
+            hexColor[i] = tempVal;
+        }
+        let hexFinal = hexColor.join('');
+        hexFinal = '#' + hexFinal;
+        return hexFinal;
+         
+    }
+
+
+
+
+
 })(); 
 
 
